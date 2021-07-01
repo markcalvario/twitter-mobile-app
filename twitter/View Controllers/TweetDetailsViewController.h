198da6ca8308f746133bce29cf5854009aad1471
Mark Anthony Calvario <mac2492@fb.com>
2021-07-01 16:36:00 -0700

    [Twitter App][Feature 6] Tweet Detail Page 	- add a detail view page when user clicks on the tweet cell 	- detail view page uses autolayout 	- add segue from timelineviewcontroller to details controller 	- updated the README.md

---
+++ twitter/View Controllers/TweetDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  TweetDetailsViewController.h
+//  twitter
+//
+//  Created by mac2492 on 6/30/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetDetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/View Controllers/TweetDetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  TweetDetailsViewController.m
+//  twitter
+//
+//  Created by mac2492 on 6/30/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import "TweetDetailsViewController.h"
+
+@interface TweetDetailsViewController ()
+
+@end
+
+@implementation TweetDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
